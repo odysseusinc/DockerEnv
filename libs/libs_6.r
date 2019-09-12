19c37281f5b2137537cb73628cf4267428fa4df5
Pavel Grafkin <pavgra@gmail.com>
2019-09-12 19:26:34 +0300

    Specifies explicit versions for all libs. Splits libs.r to take advantage of Docker layers (#7)

---
+++ libs/libs_6.r
@@ -0,0 +1,22 @@
+library("devtools")
+drat::addRepo(c("OHDSI", "cloudyr"))
+# DO NOT CHANGE ORDER OF THE FOLLOWING INSTRUCTIONS
+# Do not use install_github to install BigKnn library
+install.packages("BigKnn")
+install_github("rstudio/reticulate", ref = "f17091b", upgrade = "never") # 1.13 per https://github.com/rstudio/keras/issues/869
+install_github("OHDSI/CohortMethod", ref = "v3.0.2", upgrade = "never")
+install_github("OHDSI/DatabaseConnectorJars", ref = "e4a152e", upgrade = "never") # v1.1.0
+install_github("OHDSI/DatabaseConnector", ref = "748eb43", upgrade = "never")
+install_github("OHDSI/EmpiricalCalibration", ref = "v1.4.0", upgrade = "never")
+install_github("OHDSI/FeatureExtraction", ref = "v2.2.2", upgrade = "never")
+install_github("OHDSI/OhdsiRTools", ref = "v1.5.5", upgrade = "never")
+install_github("OHDSI/SqlRender", ref = "c6c78ff", upgrade = "never")
+install_github("OHDSI/Cyclops", ref = "v2.0.2", upgrade = "never")
+install_github("OHDSI/OhdsiSharing", ref = "v0.1.3", upgrade = "never")
+install_github("OHDSI/MethodEvaluation", ref = "98ae26e", upgrade = "never")
+install_github("OHDSI/PatientLevelPrediction", ref = "v3.0.1", upgrade = "never")
+install_bitbucket("Floooo/PythonInR", upgrade = "never")
+install_github("rstudio/keras", ref = "95ea0b5", upgrade = "never")
+
+library(keras)
+install_keras()
