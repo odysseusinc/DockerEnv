84867c105f95d2e86f3287de2d90706ad5138957
wivern <vitaly.koulakov@gmail.com>
2020-06-09 20:23:16 +0300

    Fix BigKnn version

--- libs/libs_6.r
+++ libs/libs_6.r
@@ -1,11 +1,12 @@
 library("devtools")
 drat::addRepo(c("OHDSI", "cloudyr"))
 # DO NOT CHANGE ORDER OF THE FOLLOWING INSTRUCTIONS
-# Do not use install_github to install BigKnn library
-install.packages("BigKnn")
+install_github("OHDSI/Cyclops", ref = "v2.0.3", upgrade = "never")
+install_github("OHDSI/ParallelLogger", ref = "v1.2.0", upgrade = "never")
+install_github("OHDSI/BigKnn", ref = "v0.0.3", upgrade = "never")
 install.packages("VennDiagram")
 install_github("rstudio/reticulate", ref = "f17091b", upgrade = "never") # 1.13 per https://github.com/rstudio/keras/issues/869
-install_github("OHDSI/ParallelLogger", ref = "v1.2.0", upgrade = "never")
+
 
 install_github("OHDSI/CohortMethod", ref = "8a528ac", upgrade = "never")
 install_github("OHDSI/DatabaseConnectorJars", ref = "e4a152e", upgrade = "never")
@@ -14,7 +15,6 @@ install_github("OHDSI/EmpiricalCalibration", ref = "v2.0.1", upgrade = "never")
 install_github("OHDSI/FeatureExtraction", ref = "v2.2.5", upgrade = "never")
 install_github("OHDSI/OhdsiRTools", ref = "v1.7.0", upgrade = "never") # 1.8.0 has issues with PLP KNN model
 install_github("OHDSI/SqlRender", ref = "168afb3", upgrade = "never")
-install_github("OHDSI/Cyclops", ref = "v2.0.3", upgrade = "never")
 install_github("OHDSI/OhdsiSharing", ref = "v0.1.3", upgrade = "never")
 install_github("OHDSI/ROhdsiWebApi", ref = "v0.0.3", upgrade = "never")
 install_github("OHDSI/CohortDiagnostics", ref = "1e65c04", upgrade = "never")
