20d547925fbc28a53d37be8d13c0e033aeaaf1e6
Sergey Suvorov <sergey.suvorov@odysseusinc.com>
2021-06-16 15:24:47 +0300

    version of DatabaseConnector was raised

--- libs/libs_6.r
+++ libs/libs_6.r
@@ -7,7 +7,7 @@ install_github("OHDSI/BigKnn", ref = "v1.0.0", upgrade = "never")
 install.packages("VennDiagram")
 install_github("rstudio/reticulate", ref = "1aff9d8", upgrade = "never") # 1.13 per https://github.com/rstudio/keras/issues/869
 install_github("OHDSI/CohortMethod", ref = "v4.1.0", upgrade = "never")
-install_github("OHDSI/DatabaseConnector", ref = "v4.0.0", upgrade = "never")
+install_github("OHDSI/DatabaseConnector", ref = "v4.0.2", upgrade = "never")
 install_github("OHDSI/EmpiricalCalibration", ref = "v2.1.0", upgrade = "never")
 install_github("OHDSI/FeatureExtraction", ref = "v3.1.0", upgrade = "never")
 install_github("OHDSI/OhdsiRTools", ref = "v1.9.1", upgrade = "never") # 1.8.0 has issues with PLP KNN model
